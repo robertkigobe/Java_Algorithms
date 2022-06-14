96662fe8a276005f933dc45dfb11c75972705ab2
Robert Kigobe <robertkigobe@Roberts-MacBook-Pro-2.local>
2022-06-14 15:16:11 -0400

    Print Array unordered pairs complexity

--- src/complexity/PrintUnOrderedArrPairs.java
+++ src/complexity/PrintUnOrderedArrPairs.java
@@ -1,20 +1,20 @@
 package complexity;
 
-public class PrintArrPairs {
+public class PrintUnOrderedArrPairs {
 
 	public static void main(String[] args) { //.............................O(1)
 		int[] arr = { 1, 2, 3, 4 }; //......................................O(1)
-		PrintArrPairs main = new PrintArrPairs(); //........................O(1)
+		PrintUnOrderedArrPairs main = new PrintUnOrderedArrPairs(); //........................O(1)
 		main.pairsPrint(arr); //............................................O(1)
 	}
 
 	void pairsPrint(int[] array) { //.......................................O(1)
 		for (int i = 0; i < array.length; i++) { //.........................O(n)
-			for (int j = i+1; j < array.length; j++) {//....................O(n/2)
+			for (int j = 0; j < array.length; j++) {//......................O(n)
 				System.out.println(array[i] + "," + array[j]); //...........O(1)
 			}
 		}
-														//....n*n/2 .........O(n^2)
+														//..................O(n^2)
 	}
 
 }
