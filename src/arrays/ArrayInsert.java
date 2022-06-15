d5205b140f63fba58829dbdb69cc37aebd63d6f4
Robert Kigobe <robertkigobe@Roberts-MacBook-Pro-2.local>
2022-06-14 20:35:54 -0400

    Insert into arrays

---
+++ src/arrays/ArrayInsert.java
@@ -0,0 +1,55 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArrayInsert {
+
+	public static void main(String[] args) {
+		
+		SingleDimensionalArray sda = new SingleDimensionalArray(10);
+		sda.insertValue(0, 1);
+		sda.insertValue(1, 2);
+		sda.insertValue(2, 3);
+		sda.insertValue(3, 4);
+		sda.insertValue(4, 5);
+		sda.insertValue(9, 6);
+		sda.insertValue(11, 1);
+		sda.insertValue(2, 8);
+		sda.insertValue(6, 9);
+		sda.insertValue(7, 100);
+		
+
+	}
+
+}
+
+class SingleDimensionalArray {
+
+	int[] arr = null;
+
+	public SingleDimensionalArray(int sizeOfArray) {
+
+		arr = new int[sizeOfArray];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.MIN_VALUE;
+		}
+	}
+
+	public void insertValue(int location, int valueToBeInserted) {
+
+		try {
+			if (arr[location] == Integer.MIN_VALUE) {
+				arr[location] = valueToBeInserted;
+				System.out.println("Value succesfully inserted");
+			} else {
+				System.out.println("Index already has a value");
+				
+			}
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("You may not be able to insert a value outside the bounds of the array");
+		}
+
+	}
+
+}
