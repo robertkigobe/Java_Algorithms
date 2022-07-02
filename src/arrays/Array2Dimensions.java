a42d70680da3c7b7c4a663719dc28b5d64946bf6
Robert Kigobe <robertkigobe@Roberts-MacBook-Pro-2.local>
2022-07-02 19:14:23 -0400

    create 2D arrays

---
+++ src/arrays/Array2Dimensions.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Array2Dimensions {
+	
+	//Space complexity is .............o(mn)
+	
+	private static int[][] int2DArray = {{2,4,6},{3,6,9}};   //................0(1)
+
+	public static void main(String [] args) {
+		
+		System.out.println(Arrays.deepToString(int2DArray)); //................0(1)
+		
+	}
+	
+	public void create2DArray() {
+		
+	}
+
+}
