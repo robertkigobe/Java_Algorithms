10e00a3c2eb97a8cfab9708871d2c149a2591459
Robert Kigobe <robertkigobe@Roberts-MacBook-Pro-2.local>
2022-07-10 08:51:59 -0400

    Insert into a singly linked list

--- src/linkedLists/Main.java
+++ src/linkedLists/Main.java
@@ -1,12 +1,25 @@
 package linkedLists;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		SinglyLinkedList sLL = new SinglyLinkedList();
+
+		// create linked list
 		sLL.createSinglyLinkedList(5);
-		System.out.println(sLL.head.value);
+
+		System.out.println("The value in the head node is: " + sLL.head.value);
+		System.out.println("The number of nodes is: " + sLL.size);
+
+		// insert into linked list
+		sLL.insertIntoLinkedList(7, 0);
+		System.out.println("The value in the head node is: " + sLL.head.value);
+		System.out.println("The number of nodes is: " + sLL.size);
+
+		// insert into linked list
+		sLL.insertIntoLinkedList(9, 1);
+		System.out.println("The number of nodes is: " + sLL.size);
 	}
 
 }
--- src/linkedLists/SinglyLinkedList.java
+++ src/linkedLists/SinglyLinkedList.java
@@ -6,6 +6,8 @@ public class SinglyLinkedList {
 	public Node tail;
 	public int size;
 	
+	//create a linked singly list
+	
 	public Node createSinglyLinkedList(int nodeValue) {
 		
 		head = new Node();
@@ -16,5 +18,42 @@ public class SinglyLinkedList {
 		size = 1;
 		
 		return node;
+	}
+	
+	//insert a node in a singly list
+	public void insertIntoLinkedList(int nodeValue, int location) {
+		//create node that takes the node value
+		Node node = new Node();
+		node.value = nodeValue;
+		//check if linked list is already created
+		if(head == null) {
+			createSinglyLinkedList(nodeValue);
+			return;
+			//check if inserting at first value
+		} else if (location == 0) {
+			node.next = head;
+			head = node;
+			
+			//if location is greater than size then insert node at tail of the Linked list
+		} else if (location >= size) {
+			node.next = null;
+			tail.next = node;
+			tail = node;
+			
+			//else insert at a given location
+		} else {
+			Node tempNode = head;
+			int index = 0;
+			while (index <location -1) {
+				tempNode = tempNode.next;
+				index ++;
+			}
+			Node nextNode = tempNode;
+			tempNode.next = node;
+			node.next = tempNode;
+					
+		}
+		size ++;
+		
 	}
 }
