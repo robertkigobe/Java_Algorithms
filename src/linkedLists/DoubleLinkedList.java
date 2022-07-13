b7136f93cfcee382d2d6138d2e3c285e64ac98b2
Robert Kigobe <robertkigobe@Roberts-MacBook-Pro-2.local>
2022-07-13 05:22:47 -0400

    Insert a node in  a double  linked list

--- src/linkedLists/DoubleLinkedList.java
+++ src/linkedLists/DoubleLinkedList.java
@@ -25,5 +25,47 @@ public class DoubleLinkedList {
 		return node;
 		
 	}
+	
+	//Insert into a double linked list
+	public void insertInDLL(int nodeValue, int location) {
+		DoubleLLNode node = new DoubleLLNode();
+		
+		if(head == null) {
+			createDLL(nodeValue);
+			return;
+			//Inserting at the begining of the DLL
+		} else if(location == 0) {
+			
+			
+			node.next = head;
+			node.previous = null;
+			head.previous = node;
+			head = node;
+			
+			
+		} else if (location >=size) {
+			node.next = null;
+			tail.next = node;
+			node.previous = tail;
+			tail = node;
+		} else {
+			DoubleLLNode tempNode = new DoubleLLNode();
+			int index = 0;
+			
+			while (index <location-1) {
+				tempNode = tempNode.next;
+				index++;
+			}
+			
+			node.previous = tempNode;
+			node.next = tempNode.next;
+			tempNode.next = node;
+			node.next.previous = node;
+		}
+		
+		size++;
+		
+		
+	}
 
 }
--- src/linkedLists/MainDoubleLinkedList.java
+++ src/linkedLists/MainDoubleLinkedList.java
@@ -9,6 +9,20 @@ public class MainDoubleLinkedList {
 		System.out.println("Creatig a Double linked List\n");
 		dLL.createDLL(35);
 		System.out.println("Double linked List created succesfully with size: " + dLL.size);
+		
+		// insert into linked list
+		dLL.insertInDLL(9, 1);		
+		dLL.insertInDLL(13, 2);
+		dLL.insertInDLL(10, 3);		
+		dLL.insertInDLL(8, 4);			
+		dLL.insertInDLL(7, 5);
+		dLL.insertInDLL(17, 6);
+		dLL.insertInDLL(37, 7);
+		dLL.insertInDLL(27, 8);
+		dLL.insertInDLL(57, 9);
+		dLL.insertInDLL(67, 10);
+		
+		System.out.println("Double linked List created succesfully with size: " + dLL.size);
 
 	}
 
